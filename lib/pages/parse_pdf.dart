a29947318fbe0cc22764e2866703442691f93fa1
ahmedXDR <a.soliman@innopolis.university>
2022-06-21 09:37:03 +0300

    Edit parse_pdf to support multiple file selection.

--- lib/pages/parse_pdf.dart
+++ lib/pages/parse_pdf.dart
@@ -4,20 +4,37 @@ import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:cv_parser/scripts/local_storage.dart';
+import 'package:universal_html/html.dart';
 
-Future<List> getFileBytes() async {
-  Uint8List? fileBytes;
-  String fileName = "";
+class PdfFile<T1, T2> {
+  final T1 name;
+  final T2 bytes;
 
-  FilePickerResult? picked = await FilePicker.platform
-      .pickFiles(type: FileType.custom, allowedExtensions: ['pdf']);
+  PdfFile(this.name, this.bytes);
+}
+
+Future<List<PdfFile>> getFilesBytes() async {
+  List<PdfFile> files = [];
+
+  FilePickerResult? picked = await FilePicker.platform.pickFiles(
+    // add multiple files
+    allowMultiple: true,
+    type: FileType.custom,
+    allowedExtensions: ['pdf'],
+  );
 
   if (picked != null && picked.files.isNotEmpty) {
-    fileBytes = picked.files.first.bytes;
-    fileName = picked.files.first.name;
+    // loop through files
+    for (int i = 0; i < picked.files.length; i++) {
+      // get file bytes
+      Uint8List? fileBytes = picked.files[i].bytes;
+      String fileName = picked.files[i].name;
+
+      files.add(PdfFile(fileName, fileBytes));
+    }
   }
 
-  return [fileName, fileBytes];
+  return files;
 }
 
 Future<String> textTojson(String text) async {
@@ -45,24 +62,26 @@ Future<String> mockAPI(String text) async {
   return mockResponse;
 }
 
-Future<void> parseFile() async {
+Future<void> parseFiles() async {
   //Load the PDF document.
-  final List<dynamic> file = await getFileBytes();
-  final String fileName = file[0];
-  final PdfDocument document = PdfDocument(inputBytes: file[1] as Uint8List);
+  final List<PdfFile> files = await getFilesBytes();
 
-  //Create PDF text extractor to extract text.
-  final PdfTextExtractor extractor = PdfTextExtractor(document);
+  for (int i = 0; i < files.length; i++) {
+    final String fileName = files[i].name;
+    final PdfDocument document = PdfDocument(inputBytes: files[i].bytes);
 
-  //Extract text.
-  final String text = extractor.extractText();
+    //Create PDF text extractor to extract text.
+    final PdfTextExtractor extractor = PdfTextExtractor(document);
 
-  //Dispose the document.
-  document.dispose();
+    //Extract text.
+    final String text = extractor.extractText();
 
-  //Convert text to json.
-  var json = await textTojson(text);
+    //Dispose the document.
+    document.dispose();
 
-  //save the text to local storage
-  save(fileName, json);
+    //save the text to local storage
+    var json = await mockAPI(text);
+    // var all = loadAll();
+    save(fileName, json);
+  }
 }
